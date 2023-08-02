055717810b57e297d63c984b8281f60281ea61fe
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-08-02 12:55:14 +0530

    OK

---
+++ 10_whileloops.js
@@ -0,0 +1,19 @@
+//while loops
+console.log("while loops");
+
+let i=0,n=50;
+while(i<n)
+{
+    console.log(i);
+    i++;
+}
+
+// do while loops
+console.log("do while loops");
+i=0,n=0;
+do{
+    console.log(i);
+    i++;
+}while(i<n);
+// do while loops executes atleast once
+// 0
