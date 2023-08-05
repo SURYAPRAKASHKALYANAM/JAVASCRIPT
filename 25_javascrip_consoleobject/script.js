75ef09dd3a632696372f511c4c9f113e1579a678
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-08-05 21:47:55 +0530

    ok

--- 25_javascrip_consoleobject/script.js
+++ 25_javascrip_consoleobject/script.js
@@ -1,3 +1,24 @@
 // javascript is the logic behind webpage whuch make webpages alive
 // console object 
 console.log(console);
+console.info("this is info");
+console.warn("this is warning");
+console.error("this is error");
+console.assert(4>3);
+console.assert(3>4);
+
+console.time("time taken");
+for(let i=0;i<500;i++)
+{
+
+}   
+console.timeEnd("time taken");
+
+//while loop 
+console.time("time taken1");
+let j=0;
+while(j<500)
+{
+    j++;
+}
+console.timeEnd("time taken1");
