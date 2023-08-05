41776b09320780e7fd45763332510e64373f3c48
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-08-05 22:19:31 +0530

    ok

---
+++ 27_javascript_alert,prompt,confirm/script.js
@@ -0,0 +1,14 @@
+alert("Hello World");
+let a=prompt("enter a number","0"); // this will take input from user and store it in a variable a 
+alert("you have entered "+a);
+
+
+let  b=confirm("are you sure you want to writt into  page");
+if(b)
+{
+    document.write("you have written into the page");
+}
+else
+{
+    document.write("you have cancelled the operation");
+}   
---
+++ 28_javascript_DOM,BOM,WINDOW OBJECT/script.js
@@ -0,0 +1,4 @@
+window.console.log(window);
+console.log(document.body);
+document.body.style.backgroundColor="white";
+location.href="https://www.google.com"; // this will take you to google.com 
