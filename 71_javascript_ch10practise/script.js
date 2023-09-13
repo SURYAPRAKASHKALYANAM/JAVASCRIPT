1fd10acc11ee42b1c79ab4484479b13cb9c54abc
SURYA PRAKASH KALYANAM <suryaprakashk1805@gmail.com>
2023-09-13 17:34:18 +0530

    ok

--- 71_javascript_ch10practise/script.js
+++ 71_javascript_ch10practise/script.js
@@ -1,8 +1,10 @@
 let data=fetch("https://kontests.net/api/v1/all")
 data.then((response)=>{
-    return response.json()
-}
-).then((value)=>
+    return response.json();
+}).then((value)=>
 {
-    console.log(value)
+    for(item in value)
+    {
+        console.log(value[item]["name"]);
+    }
 })
