71b7da42adecd5d26950c827b03e662494937269
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-09-14 22:42:36 +0530

    ok

---
+++ 99_javascript_regularexpression/script.js
@@ -0,0 +1,15 @@
+const regex = /very/g
+const text="I am very very very good"
+console.log(text.replace(regex,"VERY"))
+
+
+
+// Character classes
+// .	any character except newline
+// \w\d\s	word, digit, whitespace
+// \W\D\S	not word, digit, whitespace
+// [abc]	any of a, b, or c
+// [^abc]	not a, b, or c
+// [a-g]	character between a & g
+
+// REFER https://regexr.com/ FOR MORE 
