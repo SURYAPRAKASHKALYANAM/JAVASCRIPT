def02862b301be972d305f327b87e3c2b1eb739c
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-09-14 22:18:32 +0530

    ok

--- 98_javascript_express to create backend server/express.js
+++ 98_javascript_express to create backend server/express.js
@@ -1,14 +1,15 @@
 const express = require('express')
+const path=require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  console.log(req,query.name)
+  // console.log(req,query.name)
   res.send('Hello World!')
 })
 
 app.get('/about', (req, res) => {
-    res.send('about')
+  res.sendFile(path.join(__dirname,"./index.html"))
   })
 
 app.get('/contact', (req, res) => {
