c2eed8208bca81d023cc8abff5b9743d4411753b
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-09-14 21:48:51 +0530

    ok

---
+++ 97_javasccript_modules/module1.js
@@ -0,0 +1,7 @@
+const hello = () => {
+    console.log('Hello')
+}
+const Ahello=(name)=>{
+    console.log(`Hello ${name}`)
+}
+module.exports = {hello,Ahello};
---
+++ 97_javasccript_modules/module2.js
@@ -0,0 +1,15 @@
+// ES6 modules
+
+export const hello = () => {
+    console.log('Hello')
+}
+
+export const Ahello=(name)=>{
+    console.log(`Hello ${name}`)
+}
+
+const surya= ()=>{
+    console.log("surya")
+}
+
+export default surya;
