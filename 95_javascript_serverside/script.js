bbe4da2e797bb9ee7417801e7acf68329e997117
SURYAPRAKASHKALYANAM <suryaprakashk1805@gmail.com>
2023-09-14 21:05:32 +0530

    ok

---
+++ 95_javascript_serverside/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+/* JavaScript is the server-side language used to develop services for the Opera Unite feature of the Opera browser. 
+This is a server built into the browser. 
+The JavaScript API includes local file access to a virtual sandboxed file-system and persistent storage via persistent global variables. 
+Node.js is a runtime environment to allow JavaScript to not only be run in the browser, 
+but also on the server (or almost any environment, really). 
+That also expanded the types of applications that could be built with the language since it wasn't tied to only the client-side anymore.
+After that, the popularity of the language exploded.
+Node is available on Windows, Linux, Mac, and many other platforms.
+Developers started using the power of the platform, which allowed them to access APIs such as the filesystem and to use powerful applications.
+Popular projects have been created to ease the development of software, web, desktop, mobile, and other types of applications. 
+Thanks to Node, Chromium, and Electron we can now have sophisticated IDEs like 
+Visual Studio Code, programming languages like TypeScript, powerful command-line applications like React Native, and Expo to 
+create mobile apps with React.This is all possible because JavaScript is now available everywhere.*/
