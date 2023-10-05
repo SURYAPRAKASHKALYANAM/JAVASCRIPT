5a1c053ba9dbfd5fe5f6a63fe9334e635a8f8498
SURYA PRAKASH KALYANAM <suryaprakashk1805@gmail.com>
2023-10-05 10:25:40 +0530

    Done

--- 71_javascript_ch10practise/script.js
+++ 71_javascript_ch10practise/script.js
@@ -1,31 +1,13 @@
 let data=fetch("https://kontests.net/api/v1/all")
 data.then((response)=>{
     return response.json();
-}).then((value)=>
-{
-<<<<<<< HEAD
+}).then((value)=>{
+
     console.log(value)
-})
 
-
-/* ******************* NOTES APP (REMAINING QUESTIONS OF PRACTICE SET) *********** */
-// let n = localStorage.getItem("note")
-// alert("Your note is " + n)
-
-// let a = prompt("Enter your note")
-// if (a) {
-//         localStorage.setItem("note", a)
-// }
-
-// let c = confirm("Do you want to delete your note?")
-// if (c) {
-//         localStorage.removeItem("note")
-//         alert("Note deleted successfully!")
-// }
-=======
     for(item in value)
     {
         console.log(value[item]["name"]);
     }
-})
->>>>>>> 1fd10acc11ee42b1c79ab4484479b13cb9c54abc
+}
+);
